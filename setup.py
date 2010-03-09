5f33d85e412b42d58856a6f77cff7a10f9f47415
Stephane Angel <twidi@home.twidi.com>
2010-03-09 14:22:02 +0100

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-transmeta',
+    version='0.9.1',
+    url='http://github.com/twidi/django-transmeta',
+    packages=find_packages(),
+)
+
