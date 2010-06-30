ce9163c4110f6e9f1131009154a938f5fcdded76
Stephane Angel <s.angel@twidi.com>
2010-06-30 18:55:54 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-transmeta',
-    version='0.9.1',
+    version='0.9.2',
     url='http://github.com/twidi/django-transmeta',
     packages=find_packages(),
 )
